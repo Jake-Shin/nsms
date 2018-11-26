62429f2dac72093117c34a123028c8584359b98d
Jake <sinminow@gmail.com>
2018-11-26 15:01:53 +0900

    start

---
+++ nsms/src/main/java/com/nsms/NsmsApplication.java
@@ -0,0 +1,12 @@
+package com.nsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsmsApplication.class, args);
+	}
+}
